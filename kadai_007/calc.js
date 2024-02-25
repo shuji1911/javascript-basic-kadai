f447139151b8d8735399ea788791619d1b6dac48
shuji1911 <158697245+shuji1911@users.noreply.github.com>
2024-02-25 13:55:34 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,6 +1,9 @@
-let num = 3;
+let num = 15;
+if(num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です')
+}
 
-if(num % 3 == 0) {
+else if(num % 3 == 0) {
     console.log('3の倍数です')
 }
 
@@ -10,9 +13,7 @@ else if(num % 5 == 0) {
     console.log('5の倍数です')
 }
 
-else if(num % 3 == 0 && num % 5 == 0) {
-    console.log('3と5の倍数です')
-}
+
 
 else {
     console.log('num');
