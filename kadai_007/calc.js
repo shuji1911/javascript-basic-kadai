3885d045a927f81a3c0c9cf3ebe8e72e39b71af8
shuji1911 <158697245+shuji1911@users.noreply.github.com>
2024-02-25 10:44:41 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,23 @@
+let num = 3;
+
+if(num % 3 == 0) {
+    console.log('3の倍数です')
+}
+else {
+    console.log('num');
+}
+
+if(num % 5 == 0) {
+    console.log('5の倍数です')
+}
+else {
+    console.log('num');
+}
+
+if(num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です')
+}
+
+else {
+    console.log('num');
+}
